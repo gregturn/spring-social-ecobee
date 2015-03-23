14afc03a8ce62d9e32c87e4a9aa9afbd7aca78e7
Greg Turnquist <gturnquist@gopivotal.com>
2015-03-22 19:24:35 -0500

    Got Spring Social Ecobee to authorize my app

--- spring-social-ecobee-core/src/main/java/org/springframework/social/ecobee/connect/EcobeeConnectionFactory.java
+++ spring-social-ecobee-core/src/main/java/org/springframework/social/ecobee/connect/EcobeeConnectionFactory.java
@@ -1,12 +1,13 @@
 package org.springframework.social.ecobee.connect;
 
-import org.springframework.social.ecobee.api.Ecobee;
 import org.springframework.social.connect.support.OAuth2ConnectionFactory;
+import org.springframework.social.ecobee.api.Ecobee;
 
 public class EcobeeConnectionFactory extends OAuth2ConnectionFactory<Ecobee> {
 
 	public EcobeeConnectionFactory(String apiKey) {
 		super("ecobee", new EcobeeServiceProvider(apiKey), new EcobeeAdapter());
+		setScope("smartRead");
 	}
 
 }
