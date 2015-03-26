6f9c98125019967ba32ab022c491d8430d2cc130
Greg Turnquist <gturnquist@gopivotal.com>
2015-03-25 21:53:35 -0500

    Apply licensing to source code

--- spring-social-ecobee-core/src/main/java/org/springframework/social/ecobee/api/ReportSelection.java
+++ spring-social-ecobee-core/src/main/java/org/springframework/social/ecobee/api/ReportSelection.java
@@ -1,6 +1,19 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.ecobee.api;
-
-import java.util.Date;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -11,25 +24,14 @@ import org.springframework.util.StringUtils;
 public class ReportSelection {
 
 	private final SelectionType selection;
-	private final Date startDate;
-	private final Date endDate;
-	private final String columns;
 
-	public String getStartDate() {
-		return EcobeeUtility.dateToString(this.startDate);
-	}
-
-	public String getEndDate() {
-		return EcobeeUtility.dateToString(this.endDate);
-	}
-
-	public static ReportSelection thermostats(Date startDate, Date endDate, String columns, String... thermostatIds) {
+	public static ReportSelection thermostats(String... thermostatIds) {
 		return new ReportSelection(new SelectionType("thermostats",
-				StringUtils.arrayToCommaDelimitedString(thermostatIds)), startDate, endDate, columns);
+				StringUtils.arrayToCommaDelimitedString(thermostatIds)));
 	}
 
-	public static ReportSelection allThermostats(Date startDate, Date endDate, String columns) {
-		return new ReportSelection(new SelectionType("registered", ""), startDate, endDate, columns);
+	public static ReportSelection allThermostats() {
+		return new ReportSelection(new SelectionType("registered", ""));
 	}
 
 }
--- spring-social-ecobee-core/src/main/java/org/springframework/social/ecobee/api/impl/json/EcobeeModule.java
+++ spring-social-ecobee-core/src/main/java/org/springframework/social/ecobee/api/impl/json/EcobeeModule.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.social.ecobee.api.impl.json;
 
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.social.ecobee.api.EcobeeRuntime;
 import org.springframework.social.ecobee.api.EcobeeUserProfile;
 import org.springframework.social.ecobee.api.Function;
@@ -16,6 +30,11 @@ import org.springframework.social.ecobee.api.ThermostatFunction;
 import org.springframework.social.ecobee.api.ThermostatSummary;
 import org.springframework.social.ecobee.api.Thermostats;
 
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+/**
+ * @author Greg Turnquist
+ */
 public class EcobeeModule extends SimpleModule {
 
 	public EcobeeModule() {
